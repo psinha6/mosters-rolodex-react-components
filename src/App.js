94659b702f5a91f1436aac3ba403774c8032ed46
Prateek Sinha <psinha6@gmail.com>
2022-10-07 10:15:25 +0530

    React basics

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => {
+      this.setState(
+        () => {
+          return {monsters: users}
+        },
+        () => {
+          console.log(this.state);
+        }
+      );
+    });
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(
+      () => {
+        return { searchField };
+      }
+    )
+  };
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters rolodex react</h1>
+        <SearchBox className='search-box' placeholder='search mosnters' onChangeHandler={onSearchChange}/>
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
